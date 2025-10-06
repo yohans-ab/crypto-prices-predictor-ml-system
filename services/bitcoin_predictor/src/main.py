1a44febe8eb56146265bbd6daad1a459b0a36372
yohans-ab <yohans.brhane21@gmail.com>
2025-10-07 00:33:11 +0200

    Batch update: updating all services

---
+++ services/bitcoin_predictor/src/main.py
@@ -0,0 +1,63 @@
+from typing import Dict, List
+
+from loguru import logger
+from quixstreams import Application
+from src.config import config
+from kraken_api import KrakenWebSocketTradeAPI
+
+# import sys
+# sys.path.append('C:\Users\y.brhane\src\crypto-prices-predictor-ml-system\services\trade_producer')
+
+
+
+def produce_trade(kafka_broker_address: str, 
+                  kafka_topic_name: str,
+                  product_id: str,) -> None:
+    """
+    Produce trade data from the Kraken API to a Kafka topic
+
+    args:
+    kafka_broker_address: str : The address of the Kafka broker
+    kafka_topic: str : The name of the Kafka topic to produce the trade data to
+    product_id: str : The product_id to get the trade data for
+
+    return:
+    None
+    """
+    app = Application(broker_address=kafka_broker_address)
+
+    topic = app.topic(name=kafka_topic_name, value_serializer='json')
+
+    # Create an instance of the KrakenWebSocketTradeAPI
+    kraken_api = KrakenWebSocketTradeAPI(product_id=product_id)
+
+    logger.info('Creating a Producer instance')
+
+    # Create a Producer instance
+    with app.get_producer() as producer:
+        while True:
+            # get the trades from the Kraken API
+            trades: List[Dict] = kraken_api.get_trades()
+            for trade in trades:
+                # Serialize an event using the defined Topic
+                message = topic.serialize(key=trade['product_id'], value=trade)
+
+                # Produce a message into the Kafka topic
+                producer.produce(topic=topic.name, value=message.value, key=message.key)
+
+                # message = f"{message.key}: {message.value}"
+                # print(f"Produced message: {message}")
+                logger.info(trades)
+
+                # Sleep for 1 second
+                from time import sleep
+
+                sleep(1)
+
+
+if __name__ == '__main__':
+    produce_trade(
+        kafka_topic_name= 'trade',
+        kafka_broker_address= config.kafka_broker_address, 
+        product_id= config.product_id
+        )
